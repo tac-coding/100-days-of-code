932323cb899e415c45ac19022955060afc553e63
tac-coding <117896137+tac-coding@users.noreply.github.com>
2025-06-21 15:16:32 +1200

    Band Name Generator

---
+++ day01_band_name_generator/day01_band_name_generator.py
@@ -0,0 +1,9 @@
+# Program greeting
+print("Welcome to my band generator!\nJust follow the instructions to create your own band name.")
+
+# User input area
+city = input("What city did you grow up in? ")
+pet = input("What's the name of your childhood or current pet? ")
+
+# Provide band name
+print("Your band name is" " " + city + " " + pet + "!")
---
+++ day01_band_name_generator/readme.md
@@ -0,0 +1,5 @@
+# Day 01 - Band Name Generator
+
+Simple Python script that asks for a city and pet name, then generates a fun band name.  
+
+Based on a challenge from the *100 Days of Code* course by Angela Yu.
